2d51132630447d169f3129d6dcb7d387fd886133
hiteple <hiteplegold@gmail.com>
2020-04-07 17:41:00 -0300

    Implementing AddSubjects, AddStudents and AddCourses

--- App/SchoolEngine.cs
+++ App/SchoolEngine.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Etapa1.Entities;
 
 namespace Etapa1.App
@@ -15,18 +16,22 @@ namespace Etapa1.App
 
         public void Initialize()
         {
-            School.Courses = new List<Course>()
-            {
-                new Course("101", HourType.Morning),
-                new Course("201", HourType.Morning),
-                new Course("301", HourType.Morning)
-            };    
+            // Courses
+            AddCourses();
+
+            // Students generated inside AddCourses
+
+            // Subjects
+            AddSubjects();
+
+            // Exams
+            AddExams();
 
             // ADD //
             // Add
-            School.Courses.Add(new Course("102", HourType.Afternoon));
-            School.Courses.Add(new Course("202", HourType.Afternoon));
-            
+            //School.Courses.Add(new Course("102", HourType.Afternoon));
+            //School.Courses.Add(new Course("202", HourType.Afternoon));
+
             /*
             // REMOVE //
             // Remove all
@@ -58,6 +63,63 @@ namespace Etapa1.App
             School.Courses.Add(temporaryCourse4);
             School.Courses.RemoveAll(course => course.Name == "600" && course.HourType == HourType.Morning);
             */
+        }
+
+        private void AddExams()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void AddSubjects()
+        {
+            foreach (var course in School.Courses)
+            {
+                List<Subject> subjectsList = new List<Subject>()
+                {
+                    new Subject("Mathematics"),
+                    new Subject("English"),
+                    new Subject("Cience"),
+                    new Subject("Philosophy")
+                };
+                course.Subjects = subjectsList;
+            }
+        }
+        
+        private IEnumerable<Student> AddStudentsRandomly(int studentsQuantity)
+        {
+            string[] name1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] name2 = {"Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes"};
+            string[] lastname1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+
+            // Using Linq, similar to SQL (other way is to nest 3 foreach to mix all.... not gonna happen)
+            var studentsList = 
+                from n1 in name1
+                from n2 in name2
+                from l1 in lastname1
+                select new Student($"{n1} {n2} {l1}");
+
+            // Using lambda to order
+            return studentsList.OrderBy(student => student.UniqueId).Take(studentsQuantity).ToList();
+        }
+
+        private void AddCourses()
+        {
+            School.Courses = new List<Course>()
+            {
+                new Course("101", HourType.Morning),
+                new Course("201", HourType.Morning),
+                new Course("301", HourType.Morning),
+                new Course("401", HourType.Morning),
+                new Course("501", HourType.Morning)
+            };
+
+            // Generate random limit between 5 and 20
+            Random randomQty = new Random();
+            foreach (var course in School.Courses)
+            {
+                int randomNum = randomQty.Next(5, 20);
+                course.Students = AddStudentsRandomly(randomNum).ToList();
+            }
         }
         
         private static bool PredicateMethod(Course courseObj)
